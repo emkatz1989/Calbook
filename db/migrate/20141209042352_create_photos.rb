29dd493be2c1c85a1e2c53e4f9d0f28f6ae5d47f
Eric Katz <emkatz1989@gmail.com>
2014-12-09 15:07:38 -0800

    heroku take 2

--- db/migrate/20141209042352_create_photos.rb
+++ db/migrate/20141209042352_create_photos.rb
@@ -1,8 +1,8 @@
-class CreatePhotos < ActiveRecord::Migration
-  def change
-    create_table :photos do |t|
+# class CreatePhotos < ActiveRecord::Migration
+#   def change
+#     create_table :photos do |t|
 
-      t.timestamps
-    end
-  end
-end
+#       t.timestamps
+#     end
+#   end
+# end
