df0589447d5374a0abd644aae91fa2fd7f043344
Eric Katz <emkatz1989@gmail.com>
2014-12-09 15:22:08 -0800

    heroku take 4

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141209042352) do
+ActiveRecord::Schema.define(version: 20141209015720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,11 +25,6 @@ ActiveRecord::Schema.define(version: 20141209042352) do
   end
 
   add_index "entries", ["user_id"], name: "index_user_id", using: :btree
-
-  create_table "photos", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "users", force: true do |t|
     t.string   "username"
